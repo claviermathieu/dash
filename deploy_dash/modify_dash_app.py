0ef1ba67c61b6d02d17e7f924070fb605c2369d6
Mathieu Clavier <mathieu.clavier@outlook.com>
2022-02-04 21:18:16 +0100

    update

---
+++ deploy_dash/modify_dash_app.py
@@ -0,0 +1,8 @@
+
+
+def application(environ, start_response):
+    body = b'Hello world!\n'
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    start_response(status, headers)
+    return [body]
---
+++ deploy_dash/modify_flask_app.py
@@ -0,0 +1,24 @@
+# app.py
+import os
+import sys
+from flask import Flask
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+
+def application(environ, start_response):
+
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It works!\n'
+    version = 'Python %s\n' % sys.version.split()[0]
+    response = '\n'.join([message, version])
+
+
+    application = Flask(__name__)
+
+    @application.route('/')
+    def index():
+        return 'hi'
+    # return [response]
+
